c08250dbc4d530a78a033efdda997e6ea6ee6c57
Zach Brady <3282673+zzzbra@users.noreply.github.com>
2022-11-02 15:02:06 -0400

    WIP: make it work across domains

--- src/index.js
+++ src/index.js
@@ -1,53 +1,66 @@
 class FormFiller {
+  static STORAGE_KEY = "FFD";
 
   constructor () {
     if (FormFiller._instance) {
       return FormFiller._instance;
     }
-    // TODO: pass this in as an init config function
-    // collect the information that we will need to fill in all the forms 
-    this.email = prompt("Enter an email: ");
-    this.phoneNumber = prompt("Enter a phone number:");
-    this.formFields = [
-      {
-        pathname: "/",
-        formFields: {
-          email: this.email,
-          access_code: "123456",
-          password: "Test1234!",
-          first_name: "First",
-          last_name: "Lastname",
-          billing_address_line_1: "428 Franklin Ave",
-          billing_address_line_2: "2",
-          billing_address_city: "Brooklyn",
-          billing_address_state: "NY",
-          billing_address_postal_code: "11238"
+
+    const stringified = document.cookie.split("; ").find((row) => row.startsWith(FormFiller.STORAGE_KEY + "="))?.split("=")[1];
+    
+    if (stringified) {
+      this.formFields = JSON.parse(stringified);
+    } else {
+      this.email = prompt("Enter an email: ");
+      this.formFields = [
+        {
+          pathname: "/",
+          formFields: {
+            email: this.email,
+            access_code: "123456",
+            password: "Test1234!",
+            first_name: "First",
+            last_name: "Lastname",
+            billing_address_line_1: "428 Franklin Ave",
+            billing_address_line_2: "2",
+            billing_address_city: "Brooklyn",
+            billing_address_state: "NY",
+            billing_address_postal_code: "11238"
+          }
+        },
+        // stripe steps -- url is the same for all, so just include all possible inputs here
+        {
+          pathname: "/d/setup/c/",
+          formFields: {
+            email: this.email,
+            "dob-month": "02",
+            "dob-day": "26",
+            "dob-year": "1989",
+            "phone": "0000000000",
+            "id_number": "143869634",
+          }
         }
-      },
-      // stripe steps -- url is the same for all, so just include all possible inputs here
-      {
-        pathname: "/d/setup/c/",
-        formFields: {
-          phone: this.phoneNumber,
-          email: this.email,
-          "dob-month": "02",
-          "dob-day": "26",
-          "dob-year": "1989",
-          "phone": "2016653631",
-          "id_number": "143869634",
-        }
-      }
-    ];
+      ];
+    }
     FormFiller._instance = this;
-    // TODO: also set on localStorage so works across multi-domain onboarding
+    document.cookie = FormFiller.STORAGE_KEY + "=" + JSON.stringify(this.formFields);
   }
 
   fillForm () {
+    let formFields; 
+    if (this.formFields) {
+      formFields = this.formFields;
+      console.info("Retrieved form fields from memory: ", formFields);
+    } else {
+      const stringified = document.cookie.split("; ").find((row) => row.startsWith(formFields.STORAGE_KEY + "="))?.split("=")[1];
+      formFields = JSON.parse(stringified);
+      console.info("Retrieved form fields from storage: ", formFields);
+    }
     // Only assume that we match the start of the path -- this allows us to just build one set of
     // form values for a section of a site (where multiple pages have similar prefixed paths) or to
     // apply to forms where we don't have control over the end of the path (due to some nonce)
     const currentPathnameRe = new RegExp(`^${window.location.pathname}`);
-    const match = this.formFields.find(({ pathname }) => {
+    const match = formFields.find(({ pathname }) => {
       return currentPathnameRe.test(pathname);
     });
     if (match.formFields) {
